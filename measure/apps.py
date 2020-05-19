daee652946b9a0ff6731c540f143f8bea9fd8e6b
Juan Camilo G <jcguerrera@eafit.edu.co>
2020-05-19 16:11:51 -0500

    Cambios

--- measure/apps.py
+++ measure/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
 
-class MeasureConfig(AppConfig):
-    name = 'measure'
+class HygrometerConfig(AppConfig):
+    name = 'hygrometer'
+
+
+class ParticipanteConfig(AppConfig):
+    name = 'participante'
+
